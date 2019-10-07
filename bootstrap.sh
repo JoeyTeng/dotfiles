392d4e45e8b575dd91569a8f46b12a6caa5c4c26
Joey Teng <joey.teng.dev@gmail.com>
2019-10-07 23:03:32 +0000

    First attempt to integrate Oh-My-Zsh installation.
    TODO:
    - uninstall function
    - manual installation from cloned repository
    - customised theme
    - git submodule

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,49 @@ echo "Setting environment variables..."
 source .exports
 echo -e "Environment variables setted.\n"
 
+check_pkg_manager() {
+    echo "Check package manager..."
+    if [[ `which apt` ]]; then
+	PKG_MANAGER="apt-get"
+	PKG_INSTALL="apt-get install -y"
+	PKG_UPDATE="apt-get update"
+    elif [[ `which brew` ]]; then
+	PKG_MANAGER="brew"
+	PKG_INSTALL="brew install"
+	PKG_UPDATE="brew update"
+    fi
+    echo "Package manager commands:"
+    echo "- Manager $PKG_MANAGER"
+    echo "- Install $PKG_INSTALL"
+    echo "- Update $PKG_UPDATE"
+    echo "Package manager checked"
+}
+
+prepare_pkg_manager() {
+    echo "Prepare package manager..."
+    echo "- Command: $PKG_UPDATE"
+    $PKG_UPDATE
+    echo "Package manager prepared"
+}
+
+install_zsh() {
+    echo "installing zsh..."
+    $PKG_INSTALL zsh
+    echo "zsh installed"
+}
+
+install_oh_my_zsh() {
+    if [[ ! `which zsh` ]]; then
+        install_zsh
+    fi
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+}
+
 install() {
     echo "Installing..."
+    check_pkg_manager
+    prepare_pkg_manager
+    install_oh_my_zsh
 }
 
 uninstall() {
