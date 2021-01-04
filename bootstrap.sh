49e7d25b2f7cd774a615e34f13932c118663ee2b
Joey Teng <joey.teng.dev@gmail.com>
2021-01-05 00:20:51 +0800

    Add Homebrew installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,17 +6,52 @@ echo "Setting environment variables..."
 source .exports
 echo -e "Environment variables setted.\n"
 
-check_pkg_manager() {
-    echo "Check package manager..."
-    if [[ `which apt` ]]; then
-	PKG_MANAGER="apt-get"
-	PKG_INSTALL="apt-get install -y"
-	PKG_UPDATE="apt-get update"
-    elif [[ `which brew` ]]; then
-	PKG_MANAGER="brew"
-	PKG_INSTALL="brew install"
-	PKG_UPDATE="brew update"
+press_y_to_confirm() {
+    echo "$1(y/N)"
+    read input
+    if [ "$input" != "Y" ] && [ "$input" != "y" ]; then
+        return 0
+    else
+        return 1
     fi
+}
+
+install_pkg_manager() {
+    case `uname -s` in
+        Darwin*)
+            press_y_to_confirm "Install Homebrew on macOS"
+            ret=$?
+            if [ $ret -ne 0 ]; then
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+                PATH=/usr/local/bin:/opt/homebrew/bin:$PATH
+            else
+                echo "Installation cannot proceed without package manager. Exiting..."
+                exit -1
+            fi
+            ;;
+        *)
+            echo "This script cannot handle the installation of package manager on `uname -s`, exiting..."
+            exit -1
+            ;;
+    esac
+}
+
+check_pkg_manager() {
+    while [[ $PKG_MANAGER == "" ]]; do
+        echo "Check package manager..."
+        if [[ `which brew` ]]; then
+            PKG_MANAGER="brew"
+            PKG_INSTALL="brew install"
+            PKG_UPDATE="brew update"
+        elif [[ `which apt` ]] && [[ `uname -s` != "Darwin" ]]; then
+            PKG_MANAGER="apt-get"
+            PKG_INSTALL="apt-get install -y"
+            PKG_UPDATE="apt-get update"
+        else
+            install_pkg_manager
+        fi
+    done
+
     echo "Package manager commands:"
     echo "- Manager $PKG_MANAGER"
     echo "- Install $PKG_INSTALL"
